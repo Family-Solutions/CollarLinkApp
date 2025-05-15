8d61a5d121497a7f65c2f5ffcad0943151e8142d
Seba <sebalobato05@gmail.com>
2025-05-15 15:29:25 -0500

    feat: add collars page with device information cards

--- pages/collares.vue
+++ pages/collares.vue
@@ -1,3 +1,45 @@
 <template>
-    Collares
+  <div class="m-5">
+    <Button>Nuevo</Button>
+    <div class="mt-6 flex justify-content-evenly flex-wrap">
+        <Card>
+            <template #title>Collar #1048319</template>
+            <template #content>
+                <p class="w-15rem">
+                    Mascota: Tomy<br>
+                    Estado: Activo<br>
+                    Batería: 85%<br>
+                    Última conexión: Hace 10 min
+                </p>
+            </template>
+        </Card>
+        <Card>
+            <template #title>Collar #105833</template>
+            <template #content>
+                <p class="w-15rem">
+                    Mascota: Mariano<br>
+                    Estado: Inactivo<br>
+                    Batería: 20%<br>
+                    Última conexión: Hace 2 días
+                </p>
+            </template>
+        </Card>
+        <Card>
+            <template #title>Collar #1084835</template>
+            <template #content>
+                <p class="w-15rem">
+                    Mascota: Luca<br>
+                    Estado: Activo<br>
+                    Batería: 92%<br>
+                    Última conexión: Hace 5 min
+                </p>
+            </template>
+        </Card>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+</script>
