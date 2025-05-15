12b67cfee958435b44990c9dd7fa258fa216bad3
jufer10 <fcarrasco023@gmail.com>
2025-05-15 14:50:56 -0500

    feat: last perfil

--- pages/perfil.vue
+++ pages/perfil.vue
@@ -5,17 +5,30 @@
   <div class="flex justify-content-center mt-6">
     <Card>
       <template #title>
-        <img src="https://icons.veryicon.com/png/o/miscellaneous/user-avatar/user-avatar-male-5.png" 
-            alt="user header" class="w-10rem"/>
+        <div class="flex justify-content-center">
+            <img src="https://icons.veryicon.com/png/o/miscellaneous/user-avatar/user-avatar-male-5.png" 
+            alt="user header" class="w-14rem"/>
+        </div>
+        
       </template>
         
       <template #content>
-        <div class="profile-info">
-          <h2>Juan Pérez</h2>
-          <p>juan.perez@example.com</p>
-          <p>+51 999 888 777</p>
+        <div class="w-25rem flex justify-content-center">
+            <div>
+                <h2>Juan Pérez</h2>
+                <p>juan.perez@gmail.com</p>
+                <p>+51 999 888 777</p>
+            </div>
+            <div class="h-15rem"/>
         </div>
-        <Button label="Editar perfil" icon="pi pi-pencil" class="edit-button" />
+      </template>
+      <template #footer class="flex justify-content-center">
+        <div class="flex justify-content-center">
+            <div>
+                <Button label=" Editar perfil" icon="pi pi-pencil" class="block w-20rem mb-4" severity="secondary"/>
+                <Button label=" Cerrar sesión" icon="pi pi-times" class="block w-20rem mb-4" severity="warn"/>
+            </div>
+        </div>
       </template>
     </Card>
   </div>
