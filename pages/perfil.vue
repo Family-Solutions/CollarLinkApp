54b2c707b9af0b03060ef7fa1cc015b627abd1c7
jufer10 <fcarrasco023@gmail.com>
2025-05-15 14:11:52 -0500

    feat: perfil plus

--- pages/perfil.vue
+++ pages/perfil.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+</script>
+
 <template>
-    Perfil
+  <div class="flex justify-content-center mt-6">
+    <Card>
+      <template #title>
+        <img src="https://icons.veryicon.com/png/o/miscellaneous/user-avatar/user-avatar-male-5.png" 
+            alt="user header" class="w-10rem"/>
+      </template>
+        
+      <template #content>
+        <div class="profile-info">
+          <h2>Juan Pérez</h2>
+          <p>juan.perez@example.com</p>
+          <p>+51 999 888 777</p>
+        </div>
+        <Button label="Editar perfil" icon="pi pi-pencil" class="edit-button" />
+      </template>
+    </Card>
+  </div>
 </template>
+
+
